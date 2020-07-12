97df3ebaab328a72de395ced0e8d5e4ba6a2db1f
Paulpauladq <paultzq@hotmail.com>
2020-07-12 14:51:24 -0700

    feature: basic server rendering

--- ui/browser/App.jsx
+++ ui/browser/App.jsx
@@ -1,11 +1,10 @@
-/* globals React ReactDOM */
 import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Page from './Page.jsx';
+import Page from '../src/Page.jsx';
 
 const element = (
     <Router>
